2cee553d423eeffda99e514fd696723b50749edc
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-04-30 14:07:24 -0300

    correcciones

--- mi-portfolio-web/src/components/Sobre.jsx
+++ mi-portfolio-web/src/components/Sobre.jsx
@@ -3,10 +3,11 @@ const profesion = "Programador Jr.";
 
 
 function Sobre() {
-    return (<section id="sobre" className="seccion-sobre">
-        <h2>Sobre mi...</h2>
-        <p>Hola! Soy {nombre}, un {profesion}. Amo construir aplicaciones web que resuelvan problemas del mundo real.</p>
-      </section>)
+    return (
+        <section id="sobre" className="seccion-sobre">
+            <h2>Sobre mi...</h2>
+            <p>Hola! Soy {nombre}, un {profesion}, amo construir aplicaciones web que resuelvan problemas del mundo real.</p>
+        </section>)
 }
 
 export default Sobre;
