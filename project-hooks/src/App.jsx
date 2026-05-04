2396f56e5499f451435a4570e901e2fe8bb7647a
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-04 12:54:29 -0300

    usando los hooks

---
+++ project-hooks/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementar = () => {
+    setCount(count+1);
+  };
+
+  const decrementar = () => {
+    setCount(count-1);
+  };
+
+  return (
+    <div>
+      <h1>Valor actual: {count}</h1>
+      <button onClick= {decrementar}>Decrementar</button>
+      <button onClick={incrementar}>Incrementar</button>
+    </div>
+  )
+}
+
+export default App
