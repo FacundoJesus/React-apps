fc4e69a4ffb28b3736d6209243a4c904f95656dc
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-05 13:53:02 -0300

    completed

--- app-gestion_de_múltiples_contadores/src/App.jsx
+++ app-gestion_de_múltiples_contadores/src/App.jsx
@@ -1,13 +1,50 @@
 import { useState } from 'react'
-
 import './App.css'
 
 function App() {
- 
+
+  //Estado inicial de los contadores
+  const [counters, setCounters] = useState([{id:1,value:0}]);
+
+
+  //Funcion añadir contador
+  const addCounters = () => {
+    setCounters([...counters, {id:counters.length+1, value:0}]);
+  };
+
+  //Funcion quitar contador
+  const quitCounters = () => {
+    setCounters(counters.slice(0, -1));
+  };
+
+  //Funcion para incrementar
+  const incrementCounter = (id) => {
+    setCounters(counters.map( counter =>
+      counter.id === id ? {...counter, value:counter.value+1} : counter 
+    )
+    );
+  };
+
+  //Funcion para decrementar
+  const decrementCounter = (id) => {
+    setCounters(counters.map( counter =>
+      counter.id === id ? {...counter, value:counter.value-1} : counter 
+    )
+    );
+  };
 
   return (
-    <div>
-
+    <div className='app-container'>
+      <button onClick= {addCounters}>Add Counter</button>
+      <button onClick= {quitCounters}>Quit Counter</button>
+      <ul>
+        {counters.map(counter => 
+        <li key={counter.id}>
+          Counter {counter.id}: {counter.value}
+          <button onClick={() => incrementCounter(counter.id)}>Increment</button>
+          <button onClick={() => decrementCounter(counter.id)}>Decrement</button>
+        </li>)}
+      </ul>
     </div>
    
   )
