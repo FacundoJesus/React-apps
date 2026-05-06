70837562eca5168f705d48f0b26c71be7c6c3a72
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-06 10:57:05 -0300

    .

---
+++ app-MousePosition/src/App.jsx
@@ -0,0 +1,14 @@
+import { useEffect,useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>Mouse Position</h1>
+    </div>
+  )
+}
+
+export default App
