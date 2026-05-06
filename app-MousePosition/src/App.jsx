f01f7e058140978b2131f9ec154163f71855eb2d
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-06 11:42:32 -0300

    completado

--- app-MousePosition/src/App.jsx
+++ app-MousePosition/src/App.jsx
@@ -2,13 +2,26 @@ import { useEffect,useState } from 'react'
 import './App.css'
 
 function App() {
-  
 
+  const [mousePosition, setMousePosition] = useState( {
+    x: 0,
+    y: 0
+  });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({x: event.clientX, y: event.clientY})
+    };
+    window.addEventListener('mousemove',handleMouseMove);
+  },[]);
+  
   return (
     <div>
       <h1>Mouse Position</h1>
+      <h3>X: {mousePosition.x} - Y: {mousePosition.y}</h3>
     </div>
   )
+  
 }
 
 export default App
