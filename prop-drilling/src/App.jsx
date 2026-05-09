02a2f93628a91c536df4dd2388a7265ca2a418ea
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-09 10:21:28 -0300

    .

--- prop-drilling/src/App.jsx
+++ prop-drilling/src/App.jsx
@@ -1,41 +1,48 @@
+import { createContext, useContext } from 'react'
 import './App.css'
 
-function ComponentA({theme}) {
+
+
+
+function ComponentA() {
   return (
     <div style={{border: '2px solid blue', padding:'5px'}}>
       <h2>Commponent A (Child)</h2>
-      <ComponentB theme={theme}/>
+      <ComponentB />
     </div>
   )
 }
 
-function ComponentB({theme}) {
+function ComponentB() {
   return (
     <div style={{border: '2px solid green', padding:'5px'}}>
       <h2>Commponent B (Child)</h2>
-      <ComponentC theme={theme}/>
+      <ComponentC />
     </div>
   )
 }
 
-function ComponentC({theme}) {
+function ComponentC() {
+  
+  //Lo uso aqui
+  const contextValue = useContext(themeContext);
   return (
     <div style={{border: '2px solid red', padding:'5px'}}>
       <h2>Commponent C (Great-GrandChild)</h2>
-      <div>The current theme is: {theme}</div>
+      <div>The current theme is: {contextValue}</div>
     </div>
   )
 }
 
+//Creo contexto
+const themeContext = createContext('Light');
 
 function App() {
   
-  const theme = 'Dark';
-
   return (
     <div style={{border: '2px solid black', padding:'5px'}}>
       <h1>Hello (Parent)</h1>
-      <ComponentA theme={theme}/>
+      <ComponentA />
     </div>
     
   )
