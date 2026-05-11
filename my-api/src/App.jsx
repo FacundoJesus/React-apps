e4ecf08124109c9165a6c2b994f1886084f0f48f
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-11 13:23:19 -0300

    .

--- my-api/src/App.jsx
+++ my-api/src/App.jsx
@@ -1,20 +1,44 @@
 import './App.css'
 import { useEffect, useState } from 'react';
-
+import axios from 'axios';
 function App() {
 
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(json => setData(json))
-      },[]);
+
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      
+       .then((response) => {
+        console.log(response);
+        setData(response.data);
+        setLoading(false);
+        })
+
+      .catch((error) => {
+        console.log(`Error: ${error}`);
+        setError('Failed to fetch data');
+        setLoading(false);
+      });
+
+  }, []);
+
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
 
   return (
 
     <div>
       <h1>Api Rest</h1>
+
       <ul>
         {data.map((post) => (
           <li key={post.id}>
@@ -28,4 +52,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
