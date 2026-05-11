22ac37fb7db24739d611bfd1d78c03c915bc9d7a
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-11 16:59:35 -0300

    .

--- my-api/src/App.jsx
+++ my-api/src/App.jsx
@@ -2,12 +2,22 @@ import './App.css'
 import { useState } from 'react';
 import axios from 'axios';
 
-axios.interceptors.request.use(request => {
+
+//Creo instancia axios
+const api = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com/posts',
+  headers: {
+    'Authorization':'Bearer <TOKEN>',
+    'Content-Type': 'application/json'
+  }
+})
+
+api.interceptors.request.use(request => {
   console.log('Starting Request ',request);
   return request;
 })
 
-axios.interceptors.response.use(response => {
+api.interceptors.response.use(response => {
   console.log("Response ", response);
   return response;
 })
@@ -26,16 +36,16 @@ function App() {
       userId: 1
     }
 
-    axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
+    api.post('https://jsonplaceholder.typicode.com/posts', newPost)
       .then(response => {
         console.log('New Post Added: ', response.data);
         setData([response.data,...data]);
 
       })
 
-
   }
 
+  
  return (
   <div>
 
