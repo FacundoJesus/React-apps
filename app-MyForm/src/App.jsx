1d3c344f4881566575005744cc2472d4078c9ce2
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-12 19:03:50 -0300

    ...

--- app-MyForm/src/App.jsx
+++ app-MyForm/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     handleSubmit,
     formState: { errors },
     reset,
-    //watch
+    watch
   } = useForm({
     mode: 'onChange'
   });
@@ -58,14 +58,14 @@ function App() {
   };
 
   // Confirmar contraseña
-  /*const validarConfirmarPassword = (value) => {
+  const validarConfirmarPassword = (value) => {
 
     if(value !== watch('password')) {
       return 'Las contraseñas no coinciden.';
     }
 
     return true;
-  };*/
+  };
 
   return (
 
@@ -232,7 +232,7 @@ function App() {
               placeholder='Repita la contraseña'
               {...register('confirmPassword', {
                 required: 'Debe confirmar la contraseña.',
-                //validate: validarConfirmarPassword
+                validate: validarConfirmarPassword
               })}
             />
 
