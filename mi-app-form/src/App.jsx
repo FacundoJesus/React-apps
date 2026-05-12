b0a38e281391f1beda850ac957d44f95d00b39b1
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-12 12:58:29 -0300

    completed

--- mi-app-form/src/App.jsx
+++ mi-app-form/src/App.jsx
@@ -3,7 +3,9 @@ import {useForm} from "react-hook-form"
 
 function App() {
 
-  const {register, handleSubmit, formState: {errors}, reset} = useForm();
+  const {register, handleSubmit, formState: {errors}, reset, watch} = useForm({
+    mode: 'onChange'
+  });
 
   const onSubmit = (data) => {
     console.log(data); 
@@ -26,6 +28,12 @@ function App() {
     return true;
   }
 
+  const existingUserNames = ['user123', 'facu', 'john']
+  const checkIfUserNameExist = async(username) => {
+    await new Promise(resolve => setTimeout(resolve,3000));
+    return existingUserNames.includes(username);
+  }
+
 
   return (
 
@@ -43,11 +51,43 @@ function App() {
                                                 },
                                      validate: {
                                       isNotNumber: validateName,
-                                      isAdmin: validateAdmin
+                                      isAdmin: validateAdmin,
+                                      checkUserName: async(value) => {
+                                        const exist = await checkIfUserNameExist(value);
+                                        return !exist || 'Username already taken';
+                                        }
                                       }
-                                     })}>
+                                    })}>
         </input>
         {errors.name && <span style={{color:'red'}}>{errors.name.message}</span>}
+
+        <br></br>
+
+
+        <label>Password:</label>
+        <input type='password' {...register('password', {required: 'Password is required.', 
+                                             minLength: {value:2,
+                                                         message: 'Password should be atleast 2 characters.'
+                                                        }
+                                })}>
+        </input>
+        {errors.password && <span style={{color:'red'}}>{errors.password.message}</span>}
+
+        <br></br>
+
+
+        <label>Confirm Password:</label>
+        <input type='password' {...register('confirmPassword', {required: 'Confirm Password is required.', 
+                                             minLength: {value:2,
+                                                         message: 'Confirm Password should be atleast 2 characters.'
+                                                        },
+                                             validate: value => value === watch('password') || 'Passwords do not match.'
+                                })}>
+        </input>
+        {errors.confirmPassword && <span style={{color:'red'}}>{errors.confirmPassword.message}</span>}
+
+        <br></br>
+
 
         <label>Email:</label>
         <input{...register('email', {required:'Email is required',
@@ -57,10 +97,16 @@ function App() {
         </input>
         {errors.email && <span style={{color:'red'}}>{errors.email.message}</span>}
 
+        <br></br>
+
         <label>DNI:</label>
         <input {...register('dni')}></input>
+        <br></br>
         
         <button type='submit'>Submit</button>
+
+        <br></br>
+
         <button type='button' onClick={() => reset()}>Reset</button>
 
       </form>
