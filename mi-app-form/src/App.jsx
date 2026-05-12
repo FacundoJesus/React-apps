98d5bfeefceb206c187d47d0c69c367de41986fc
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-12 09:31:55 -0300

    .

--- mi-app-form/src/App.jsx
+++ mi-app-form/src/App.jsx
@@ -2,11 +2,39 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [formData, setFormData] = useState( {
+    name: '',
+    email:''
+  })
   
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data Submitted', formData);
+
+  }
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value})
+  }
 
   return (
+
     <div>
       <h1>Forms in React</h1>
+
+      <form onSubmit={handleSubmit}>
+
+        <label>Name:</label>
+        <input type='text' name='name' value={formData.name} onChange={handleChange}></input>
+
+        <label>Email:</label>
+        <input type='text' name='email' value={formData.email} onChange={handleChange}></input>
+
+        <button type='submit'>Submit</button>
+
+      </form>
+      
     </div>
     
   )
