e9043c4f94235cd8a3b54d6544c26469584f5031
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-12 09:48:10 -0300

    .

--- mi-app-form/src/App.jsx
+++ mi-app-form/src/App.jsx
@@ -9,13 +9,41 @@ function App() {
   })
   
   const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form Data Submitted', formData);
 
+    e.preventDefault();
+
+    const validationErrors = validate();
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    }
+    else {
+      console.log('Form Data Submitted', formData);
+    }
+    
   }
 
   const handleChange = (e) => {
+    const {name,value} = e.target;
+
     setFormData({...formData, [e.target.name]: e.target.value})
+
+    if(errors[name]) {
+      const newErrors = {...errors};
+      delete newErrors[name];
+      setErrors(newErrors);
+    }
+  }
+
+  // Validaciones
+  const [errors,setErrors] = useState({});
+
+  const validate = () => {
+    const newErrors = {};
+    if(!formData.name.trim()) {
+      newErrors.name = 'Name is required';
+    }
+    return newErrors;
   }
 
   return (
@@ -27,14 +55,17 @@ function App() {
 
         <label>Name:</label>
         <input type='text' name='name' value={formData.name} onChange={handleChange}></input>
+        {errors.name && <span>{errors.name}</span>}
+        <br></br>
 
         <label>Email:</label>
         <input type='text' name='email' value={formData.email} onChange={handleChange}></input>
+        
 
         <button type='submit'>Submit</button>
 
       </form>
-      
+
     </div>
     
   )
