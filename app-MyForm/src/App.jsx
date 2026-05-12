8ebe09bd7f3334c6f1bb316478608dcbdb5af38f
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-12 17:48:24 -0300

    .

--- app-MyForm/src/App.jsx
+++ app-MyForm/src/App.jsx
@@ -13,13 +13,26 @@ function App() {
   }
 
   // Validaciones
-  const validateName = (nombre) => {
+  const validarApellido = (apellido) => {
+    if(!isNaN(apellido)) {
+      return 'El nombre no debe ser un número.';
+    }
+    return true;
+  }
+
+  const validarNombre = (nombre) => {
     if(!isNaN(nombre)) {
       return 'El nombre no debe ser un número.';
     }
     return true;
   }
 
+  const validarConfirmarContraseña = (confirmarContraseña) => {
+    if(confirmarContraseña !== watch('password')) {
+      return 'La contraseña no coincide.'
+    }
+    return true;
+  }
 
 
   return (
@@ -35,11 +48,59 @@ function App() {
                                                  message: 'El nombre debe tener al menos 4 caracteres.'
                                                 },
                                      validate: {
-                                                isNotNumber: validateName,
+                                                isNotNumber: validarNombre,
                                                }
                                     })}>
        </input>
        {errors.name && <span style={{color:'red'}}>{errors.name.message}</span>}
+
+       <label>Apellido:</label>
+       <input {...register('apellido', {required: 'El apellido es requerido.', 
+                                        minLength: {value: 4,
+                                                    message: 'El nombre debe tener al menos 4 caracteres.'
+                                                },
+                                        validate: {isNotNumber: validarApellido,
+                                               }
+                                    })}>
+       </input>
+       {errors.apellido && <span style={{color:'red'}}>{errors.apellido.message}</span>}
+
+       <label>Email:</label>
+       <input {...register('email', {required: 'El email es requerido.', 
+                                     pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                                message: 'El Email esta mal formulado.'
+                                              }
+                                    })}>
+       </input>
+       {errors.email && <span style={{color:'red'}}>{errors.email.message}</span>}
+
+       <label>DNI:</label>
+       <input {...register('dni', {required: 'El DNI es requerido.', 
+                                     pattern: { value: /^\d{7,8}$/,
+                                                message: 'El DNI debe tener entre 7 y 8 números.'
+                                              }
+                                    })}>
+       </input>
+       {errors.dni && <span style={{color:'red'}}>{errors.dni.message}</span>}
+
+       <label>Contraseña:</label>
+       <input {...register('password', {required: 'La contraseña es requerida.', 
+                                     pattern: { value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#_-])[A-Za-z\d@$!%*?&.#_-]{8,}$/,
+                                                message: 'Debe tener mayúscula, minúscula, número y carácter especial.'
+                                              }
+                                    })}>
+       </input>
+       {errors.password && <span style={{color:'red'}}>{errors.password.message}</span>}
+
+       <label>Confirmar contraseña:</label>
+       <input {...register('confirmPassword', {required: 'La contraseña es requerida.', 
+                                               pattern: { value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.#_-])[A-Za-z\d@$!%*?&.#_-]{8,}$/,
+                                                          message: 'Debe tener mayúscula, minúscula, número y carácter especial.'
+                                              },
+                                              validate: validarConfirmarContraseña
+                                    })}>
+       </input>
+       {errors.confirmPassword && <span style={{color:'red'}}>{errors.confirmPassword.message}</span>}
         
 
 
