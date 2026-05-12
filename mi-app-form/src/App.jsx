d77a882b91362d68d62eea7a70dcd2d5335ce087
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-12 10:19:38 -0300

    .

--- mi-app-form/src/App.jsx
+++ mi-app-form/src/App.jsx
@@ -1,65 +1,26 @@
-import { useState } from 'react'
 import './App.css'
+import { useForm} from "react-hook-form"
 
 function App() {
 
-  const [formData, setFormData] = useState( {
-    name: '',
-    email:''
-  })
-  
-  const handleSubmit = (e) => {
+  const {register, handleSubmit} = useForm();
 
-    e.preventDefault();
-
-    const validationErrors = validate();
-
-    if (Object.keys(validationErrors).length > 0) {
-      setErrors(validationErrors);
-    }
-    else {
-      console.log('Form Data Submitted', formData);
-    }
-    
-  }
-
-  const handleChange = (e) => {
-    const {name,value} = e.target;
-
-    setFormData({...formData, [e.target.name]: e.target.value})
-
-    if(errors[name]) {
-      const newErrors = {...errors};
-      delete newErrors[name];
-      setErrors(newErrors);
-    }
-  }
-
-  // Validaciones
-  const [errors,setErrors] = useState({});
-
-  const validate = () => {
-    const newErrors = {};
-    if(!formData.name.trim()) {
-      newErrors.name = 'Name is required';
-    }
-    return newErrors;
-  }
+  const onSubmit = (data) => console.log(data); 
 
   return (
-
     <div>
       <h1>Forms in React</h1>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
 
         <label>Name:</label>
-        <input type='text' name='name' value={formData.name} onChange={handleChange}></input>
-        {errors.name && <span>{errors.name}</span>}
-        <br></br>
+        <input{...register('name')}></input>
 
         <label>Email:</label>
-        <input type='text' name='email' value={formData.email} onChange={handleChange}></input>
+        <input{...register('email')}></input>
+
+        <label>DNI:</label>
+        <input {...register('dni')}></input>
         
 
         <button type='submit'>Submit</button>
@@ -67,7 +28,6 @@ function App() {
       </form>
 
     </div>
-    
   )
 }
 
