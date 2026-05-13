a2157abfa7919fbe63e1d7a7e34361a1d239a849
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-13 09:41:49 -0300

    .

---
+++ app-react-tailwind/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+
+    <div className='bg-orange-400 p-8 rounded-lg shadow-lg items-center'>
+      <h1 class="text-3xl font-bold ">
+        Hello world!
+      </h1>
+      <p className='text-gray-50'>
+        This is a simple example using Tailwind CSS in React Project.
+      </p>
+    </div>
+
+  )
+}
+
+export default App
