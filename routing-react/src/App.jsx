d4ec46062025e5a11271c71f63a909dd837fb254
Facundo Jesús Citera <facundojesus10@hotmail.com>
2026-05-14 11:20:28 -0300

    .

--- routing-react/src/App.jsx
+++ routing-react/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Home'  
+import About from './About'
+import Contact from './Contact'
+
+/*
+-> Home
+-> About
+-> Contact
+*/
 
 function App() {
 
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+    </Router>
   )
 }
 
